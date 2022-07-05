17c9ea16ded3d443bcef6860f423927c258bcc24
ajitvaniya <vaniyaajit@gmail.com>
2022-07-06 05:21:34 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Authentication Using Localstorage
+      </header>
+    </div>
+  );
+}
+
+export default App;
