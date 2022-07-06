8bda618158d312dfaf2edfd09ad6edf3fe5a045f
ajitvaniya <vaniyaajit@gmail.com>
2022-07-06 08:09:11 +0530

    Added Data table

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,7 +1,8 @@
 import { Button, PageHeader } from "antd";
 import React, { useEffect } from "react";
 import { useHistory } from "react-router";
-     
+//import DataTable from './components/DataTable' 
+
 export function Dashboard() {
  
   let history = useHistory();
@@ -9,9 +10,28 @@ export function Dashboard() {
   // useEffect hook for api call to set data
   useEffect(() => {}, []);
 
-  const logoutCall = () => {
+  const logoutCall = () => { 
+     localStorage.removeItem('authUser');
      history.push("/");
   };
+
+  const columns = [
+    { accessor: 'name', label: 'Name' },
+    { accessor: 'age', label: 'Age' },
+    { accessor: 'is_manager', label: 'Manager' },
+    { accessor: 'start_date', label: 'Start Date' },
+  ]
+
+  const rows = [
+    { id: 1, name: 'Liz Lemon', age: 36, is_manager: true, start_date: '02-28-1999' },
+    { id: 2, name: 'Jack Donaghy', age: 40, is_manager: true, start_date: '03-05-1997' },
+    { id: 3, name: 'Tracy Morgan', age: 39, is_manager: false, start_date: '07-12-2002' },
+    { id: 4, name: 'Jenna Maroney', age: 40, is_manager: false, start_date: '02-28-1999' },
+    { id: 5, name: 'Kenneth Parcell', age: Infinity, is_manager: false, start_date: '01-01-1970' },
+    { id: 6, name: 'Pete Hornberger', age: 42, is_manager: true, start_date: '04-01-2000' },
+    { id: 7, name: 'Frank Rossitano', age: 36, is_manager: false, start_date: '06-09-2004' },
+    { id: 8, name: null, age: null, is_manager: null, start_date: null },
+  ]
 
   return (
     <div className="dashboard">
@@ -24,7 +44,10 @@ export function Dashboard() {
           </Button>,
         ]}
       > 
-        <h2>Dashboard</h2>
+        <h1>Data</h1>
+ 
+        {/* <DataTable rows={rows} columns={columns} /> */}
+
       </PageHeader>
     </div>
   );
